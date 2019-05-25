34769a5f0391fbe9e8e83dd6cf7c33d3fda440f4
Joy Lin <vvlnote@hotmail.com>
2019-05-25 01:27:43 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -9,12 +9,31 @@ class AuthorsController < ApplicationController
 
   def create
     @author = Author.create(author_params)
-
-    redirect_to author_path(@author)
+    if @author.valid? 
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
+  end
+  
+  def edit
+    @author = Author.find(params[:id])
+  end
+  
+  def update
+    @author = Author.find(params[:id])
+    if @author.valid? && @author.update(author_params)
+      @author.save
+      redirect_to author_path(@author)
+    else
+      render :edit
+    end
   end
 
   private
 
+
+  
   def author_params
     params.permit(:name, :email, :phone_number)
   end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,20 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+  
+  def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.create(post_params)
+    if @post.valid?
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  
+  end
 
   def edit
     @post = Post.find(params[:id])
@@ -9,10 +23,12 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    if @post.valid? && @post.update(post_params)
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
 
   private
